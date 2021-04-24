1466f5c397ba1f34f3988c154c5ce06a92393c9b
Kevs <k.tapiabaeza.it@gmail.com>
2021-04-24 04:25:09 -0400

    HomePage component started

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const RestaurantList = () => {
+    return (
+        <div className="list-group">
+            <table className="table table-hover table-dark">
+                <thead>
+                    <tr className="bg-primary">
+                        <th scope="col">Restaurant</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Price Range</th>
+                        <th scope="col">Ratings</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mcdonal</td>
+                        <td>NewYork</td>
+                        <td>$$</td>
+                        <td>Rating</td>
+                        <td>
+                            <button className="btn btn-warning">Update</button>
+                        </td>
+                        <td>
+                        <button className="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { AddRestaurants } from '../components/AddRestaurants'
+import { Header } from '../components/Header'
+import { RestaurantList } from '../components/RestaurantList'
 
 export const Home = () => {
     return (
         <div>
-            Home
+            <Header/>
+            <AddRestaurants/>
+            <RestaurantList/>
         </div>
     )
 }
